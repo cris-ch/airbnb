b1db2c39e5a99b20c8abcef52bd67409fab025b0
Cristian <cristian.christensen@gmail.com>
2023-05-24 11:00:10 +0200

    Added Logo to NavBar

---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className=''>Search</div>
+  )
+}
+
+export default Search
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,27 @@
-import './globals.css'
-import { Inter, Nunito } from 'next/font/google'
+import "./globals.css";
+import { Inter, Nunito } from "next/font/google";
+import NavBar from "./components/navbar/NavBar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'APlus BnB',
-  description: 'APlus BnB is a new way to travel and stay in style.',
-}
+  title: "APlus BnB",
+  description: "APlus BnB is a new way to travel and stay in style.",
+};
 
-const font = Nunito({ subsets: ['latin'] })
+const font = Nunito({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <NavBar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
