6716703de66f0ff5732d2df1404f91f72f900e8a
Cristian <cristian.christensen@gmail.com>
2023-05-24 11:15:17 +0200

    Added a Search Bar to NavBar

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Logo from "./Logo";
 import Search from "./Search";
 
+
 const NavBar = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
