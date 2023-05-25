e3eb008fdbc70fdee392cdc00db9dd41924ba9e3
Cristian <cristian.christensen@gmail.com>
2023-05-25 11:14:08 +0200

    Added Profile section to UserMenu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import React from "react";
+import React, {useCallback, useState} from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);  
+  const toggleOpen = useCallback(() => setIsOpen((prev) => !prev), []);
+
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
@@ -25,7 +28,7 @@ const UserMenu = () => {
             Become a Host
           </div>
           <div 
-            onClick={() => {}}
+            onClick={toggleOpen}
             className="
               p-4
               md:py-1
@@ -47,6 +50,26 @@ const UserMenu = () => {
               </div>
             </div>
       </div>
+
+      {isOpen && (
+        <div 
+          className="
+            absolute
+            rounded-xl
+            shadow-md
+            w-[40vm]
+            md:w-3/4
+            bg-white
+            overflow-hidden
+            right-0
+            top-12
+            text-sm
+          ">
+            <div className="flex flex-col cursor-pointer">
+              
+            </div>
+          </div>
+      )}
     </div>
   );
 };
